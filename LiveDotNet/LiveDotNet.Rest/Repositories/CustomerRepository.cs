633ce4b724437963d90f9e679996462a187d3de0
Thyago <t.y_josef7@hotmail.com>
2020-12-20 14:14:57 -0300

    Acrescentando interface db

---
+++ LiveDotNet/LiveDotNet.Rest/Repositories/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using LiveDotNet.Rest.Domain.Etites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace LiveDotNet.Rest.Repositories
+{
+    public class CustomerRepository
+    {
+        public IEnumerable<Customer> GetAll()      //é um tipo primitivo sempre opte por usalo pois torna nem necessaria o uso do garbageCOlector torna maus facil o alocamento na memoria.
+        {
+        }
+
+
+        public void Save(Customer customer)
+        {
+
+        }
+    }
+}
