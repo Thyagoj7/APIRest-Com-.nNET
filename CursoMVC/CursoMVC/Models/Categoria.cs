aea04873025321480fb895fdd70461830c7c24ec
Thyago <t.y_josef7@hotmail.com>
2021-02-08 11:06:50 -0300

    Subindo API

--- CursoMVC/CursoMVC/Models/Categoria.cs
+++ CursoMVC/CursoMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace CursoMVC.Models
     public class Categoria
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
 
         public string Descricao { get; set; }
 
--- CursoMVC/CursoMVC/Models/Produto.cs
+++ CursoMVC/CursoMVC/Models/Produto.cs
@@ -10,7 +10,9 @@ namespace CursoMVC.Models
     {
         public int Id { get; set; }
         [Display(Name = "Descrição")]
+        
         public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa")]
 
         public int Quantidade { get; set; }
 
